788cfa3bf1f556cdd08aa37bcc085ad4f7c50091
Guilherme Lindner <lindner.guilherme@gmail.com>
2021-06-14 19:08:47 -0700

    new type: dependent

--- supplant.py
+++ supplant.py
@@ -10,6 +10,8 @@ class Configuration:
         self.read_files(self.skeleton)
         self.constants = {}
         self.variables = {}
+        self.dependents = {}
+        self.relations = {}
 
     def read_files(self, skeleton):
         all_files = glob.glob(f'{skeleton}/**', recursive=True)
@@ -25,10 +27,22 @@ class Configuration:
     def add_variable(self, key, values):
         self.variables[key] = values
 
+    def add_dependent(self, key, value, parent):
+        self.dependents[key] = value
+        self.relations[key] = parent
+
     def replace_words(self, string, words):
         for key, value in words.items():
             string = string.replace(key, str(value))
         return string
+
+    def check(self, pattern='__'):
+        for file_ in self.files:
+            with open(file_, 'r') as f:
+                for line in f:
+                    if pattern in line:
+                        print(f'{file_}: {line}')
+
 
     def write_case_files(self, id_):
         case_name = f'case_{id_}'
@@ -39,37 +53,66 @@ class Configuration:
         for file_ in self.files:
             content = open(file_, 'r').read()
             content = self.replace_words(content, self.constants)
-            new_file = file_.replace(self.skeleton, case_name)
+            if self.skeleton[-1] == '/':
+                new_file = file_.replace(self.skeleton[:-1], case_name)
+            else:
+                new_file = file_.replace(self.skeleton, case_name)
             with open(new_file, 'w') as f:
                 f.write(content)
 
     def write_configurations(self):
         id_ = 0
         var_names = []
+
         for key, value in self.variables.items():
             var_names.append(key)
 
-        if len(self.variables) == 1:
-            for i in self.variables[var_names[0]]:
+        if len(self.variables) == 0:
+            self.write_case_files(0)
+
+        elif len(self.variables) == 1:
+            for count, i  in enumerate(self.variables[var_names[0]]):
+                print(i, var_names[0])
                 self.constants[var_names[0]] = i
+                for dep_key, dep_value in self.dependents.items():
+                    if self.relations[dep_key] == var_names[0]:
+                        self.constants[dep_key] = dep_value[count_i]
+                    else:
+                        print(f'Wrong parent {self.relations[dep_key]}')
                 self.write_case_files(id_)
                 id_ += 1
 
         elif len(self.variables) == 2:
-            for i in self.variables[var_names[0]]:
-                for j in self.variables[var_names[1]]:
+            for count_i, i in enumerate(self.variables[var_names[0]]):
+                for count_j, j in enumerate(self.variables[var_names[1]]):
                     self.constants[var_names[0]] = i
                     self.constants[var_names[1]] = j
+                    for dep_key, dep_value in self.dependents.items():
+                        if self.relations[dep_key] == var_names[0]:
+                            self.constants[dep_key] = dep_value[count_i]
+                        elif self.relations[dep_key] == var_names[1]:
+                            self.constants[dep_key] = dep_value[count_j]
+                        else:
+                            print(f'Wrong parent: {self.relations[dep_key]}')
                     self.write_case_files(id_)
                     id_ += 1
 
         elif len(self.variables) == 3:
-            for i in self.variables[var_names[0]]:
-                for j in self.variables[var_names[1]]:
-                    for k in self.variables[var_names[2]]:
+            for count_i, i in enumerate(self.variables[var_names[0]]):
+                for count_j, j in enumerate(self.variables[var_names[1]]):
+                    for count_k, k in enumerate(self.variables[var_names[2]]):
                         self.constants[var_names[0]] = i
                         self.constants[var_names[1]] = j
                         self.constants[var_names[2]] = k
+                        for dep_key, dep_value in self.dependents.items():
+                            if self.relations[dep_key] == var_names[0]:
+                                self.constants[dep_key] = dep_value[count_i]
+                            elif self.relations[dep_key] == var_names[1]:
+                                self.constants[dep_key] = dep_value[count_j]
+                            elif self.relations[dep_key] == var_names[2]:
+                                self.constants[dep_key] = dep_value[count_k]
+                            else:
+                                print(f'Wrong parent: {self.relations[dep_key]}')
                         self.write_case_files(id_)
                         id_ += 1
         else:
